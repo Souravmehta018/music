4d1885bb11cf0548e1d6a8032e3a7ef0120aa931
SOURAV MEHTA <mehtas018@gmail.com>
2024-03-04 22:59:22 +0000

    music3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import HeroSection from '../components/ui/HeroSection';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased text-white bg-grid-white/[0.02]">
-        <h1 className="text-2xl text-cnter"> Music anthem </h1>
+    <main className="w-full min-h-screen bg-black/[0.96] antialiased text-white 
+    justify-center bg-grid-white/[0.02]">
         <HeroSection />
     </main>
   );
--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -1,18 +1,26 @@
 import Link from "next/link";
+import { Spotlight } from "./Spotlight";
 
 export default function HeroSection() {
     return (
         <div className="relative w-full h-auto md:h-[40rem] rounded-md flex flex-col 
-        justify-center items-center overflow-hidden mx-auto py-10 bg-grid-white/[0.02]">
+        justify-center  overflow-hidden mx-auto py-10 bg-grid-white/[0.02]">
+      <Spotlight
+        className="-top-40 left-0 md:left-20 md:-top-20"
+        fill="white"
+      />            
             
-            <div className="p-4 relative z-10 w-full text-cnter">
+            <div className="p-4 relative z-10 w-full">
                 <h1 className="text-4xl mt-20 md:mt-0 md:text-7xl font-bold bg-clip-text text-transparent 
-                bg-gradient-to-b from-neutral-50 to-neutral-400">
+                bg-gradient-to-b from-neutral-50 to-neutral-400 text-center">
                      Master The Art Of Music 
                 </h1>
-                <p className="text-sm text-cnter"> Learn to play the guitar, 
-                sing and danceLearn to play the guitar, 
-                sing and danceLearn to play the guitar, sing and dance. Learn to play the guitar, sing and dance </p>
+                <p className="mt-4 font-normal text-base text-center md:text-lg text-neutral-300 max-w-lg mx-auto">
+                    Learn to play the guitar, 
+                    sing and danceLearn to play the guitar, 
+                    sing and danceLearn to play the guitar,
+                    sing and dance. Learn to play the guitar, sing and dance
+                    . Learn to play the guitar, sing and dance </p>
                 <div className="mt-4">
                     <Link href={"/cources"}>
                     Explore Cources
