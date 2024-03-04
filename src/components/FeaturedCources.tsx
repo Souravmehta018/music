87b0abe9323e3dbe974c0d0107efbf49d4e1c72f
SOURAV MEHTA <mehtas018@gmail.com>
2024-03-04 23:52:28 +0000

    m4

--- src/components/FeaturedCources.tsx
+++ src/components/FeaturedCources.tsx
@@ -2,7 +2,9 @@ import Link from 'next/link'
 import courseData from '../data/music_courses.json'
 
 
+
 export default function FeaturedCources() {
+    courseData.courses.filter((course) => course.isFeatured)
     return (
         <div className="py-12 bg-gray-900">
             <div>
@@ -13,7 +15,12 @@ export default function FeaturedCources() {
                     text-white sm:text-4xl">Learn with the best</p>
                 </div>
             </div>
-            <div>2</div>
+            <div className='mt-10'>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+                    Test
+                </div>
+
+            </div>
             <div className='mt-20 text-center'>
                 <Link href='/cources'>
                 <button className="bg-white dark:bg-black text-black py-2 px-10 rounded-md border-2 border-radius-50
