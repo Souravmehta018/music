9795ea66f8b0a85b796dc1a6e2b7815242fad6c6
SOURAV MEHTA <mehtas018@gmail.com>
2024-03-04 12:03:24 +0000

    music

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={inter.className}>
+        <div className="relative w-full flex justify-center items-center ">
         <Navbar />
-        {children}</body>
+        {children}
+        </div>
+        </body>
+
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <h1> hELLO WORLD </h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased text-white bg-grid-white/[0.02]">
+        <h1 className="text-2xl text-cnter"> Music anthem </h1>
+    </main>
   );
 }
